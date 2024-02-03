5f01b3414f52ff4fe64cc8e6085ae289af7e00da
Yash <yashpurani8@gmail.com>
2024-02-03 13:55:31 +0530

    minor fix

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -6,16 +6,20 @@ export const getServerSideUser = async (
     cookies: NextRequest['cookies'] | ReadonlyRequestCookies
 ) => {
     const token = cookies.get('payload-token')?.value
+    try {
+        const meRes = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`, {
+            headers: {
+                Authorization: `JWT ${token}`
+            }
+        })
 
-    const meRes = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`, {
-        headers: {
-            Authorization: `JWT ${token}`
+        const { user } = (await meRes.json()) as { user: User | null }
+
+        return {
+            user
         }
-    })
-
-    const { user } = (await meRes.json()) as { user: User | null }
-
-    return {
-        user
+    } catch (error) {
+        return null
     }
+
 }
