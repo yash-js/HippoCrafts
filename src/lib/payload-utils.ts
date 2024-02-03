9f2f3ac1436eaaf42dc4daecc94be99b1d8c3415
Yash <yashpurani8@gmail.com>
2024-02-03 14:08:23 +0530

    Revert "minor fix"
    This reverts commit 5f01b3414f52ff4fe64cc8e6085ae289af7e00da.

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -6,20 +6,16 @@ export const getServerSideUser = async (
     cookies: NextRequest['cookies'] | ReadonlyRequestCookies
 ) => {
     const token = cookies.get('payload-token')?.value
-    try {
-        const meRes = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`, {
-            headers: {
-                Authorization: `JWT ${token}`
-            }
-        })
 
-        const { user } = (await meRes.json()) as { user: User | null }
-
-        return {
-            user
+    const meRes = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`, {
+        headers: {
+            Authorization: `JWT ${token}`
         }
-    } catch (error) {
-        return null
-    }
+    })
 
+    const { user } = (await meRes.json()) as { user: User | null }
+
+    return {
+        user
+    }
 }
